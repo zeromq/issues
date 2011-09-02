9a579f66309f14376b15518180884d8ed492431c
Pieter Hintjens <ph@imatix.com>
2011-09-02 14:57:29 +0200

    Added 3.0 test version

---
+++ 218/3-0/child.c
@@ -0,0 +1,31 @@
+//  Child test, run from within parent
+
+#include "zmq.h"
+#include "assert.h"
+#include "stdint.h"
+#include "stdio.h"
+#include "unistd.h"
+
+int main ()
+{
+    void *ctx = zmq_init (1);
+    assert (ctx);
+
+    void *dealer = zmq_socket (ctx, ZMQ_DEALER);
+    assert (dealer);
+    int rc = zmq_connect (dealer, "tcp://localhost:2211");
+    assert (rc == 0);
+
+    while (1) {
+        //  Send signal to parent
+        zmq_msg_t msg;
+        zmq_msg_init (&msg);
+        zmq_sendmsg (dealer, &msg, 0);
+        zmq_msg_close (&msg);
+
+        //  Do some work
+        sleep (1);
+    }
+    return 0;
+}
+
